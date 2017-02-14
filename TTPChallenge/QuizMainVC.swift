9b15aeb9600d8684364e1b82ed94d1ed0d3bc139
Luna An <mimicatcodes@gmail.com>
2017-02-14 00:40:58 -0500

    Views being created for quiz part 1

---
+++ TTPChallenge/QuizMainVC.swift
@@ -0,0 +1,34 @@
+//
+//  QuizMainVC.swift
+//  TTPChallenge
+//
+//  Created by Luna An on 2/14/17.
+//  Copyright © 2017 TeamMDC. All rights reserved.
+//
+
+import UIKit
+
+class QuizMainVC: UIViewController {
+
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var partOneCircle: UILabel!
+    @IBOutlet weak var partTwoCircle: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Styling elements
+        startButton.layer.cornerRadius = 20
+        partOneCircle.layer.cornerRadius = partOneCircle.frame.width/2
+        partOneCircle.layer.masksToBounds = true
+        partTwoCircle.layer.cornerRadius = partTwoCircle.frame.width/2
+        partTwoCircle.layer.masksToBounds = true
+        
+        // Trasnparent navigation bar
+        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
+        self.navigationController?.navigationBar.shadowImage = UIImage()
+        self.navigationController?.navigationBar.isTranslucent = true
+
+    }
+
+}
