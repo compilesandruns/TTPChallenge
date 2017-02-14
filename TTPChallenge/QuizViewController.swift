d767b92c17dcb7fe6be22da5c5e56a423dbb5133
Luna An <mimicatcodes@gmail.com>
2017-02-13 20:46:26 -0500

    local changed being saved

--- TTPChallenge/QuizViewController.swift
+++ TTPChallenge/QuizViewController.swift
@@ -8,12 +8,26 @@
 
 import UIKit
 
-class QuizzViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+struct Quiz {
+    
+    var question: String
+    var isYes : Bool = false
+    
+}
+
+class QuizViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var resultButton: UIButton!
     
-    let dummyData = ["Enjoy trying out new products", "Stay up to date on the latest and greatest in tech and devices","Want to get involved in tech, but not sure where to start", "Enjoy puzzles and challenges", "Enjoy tinkering with computers and devices","Are curious about how tech products work", "Enjoy fast-paced work", "Enjoy solving problems", "Have a creative spirit",
+    var yesCounter = 0
+    var noCounter = 0
+    var isYes = false
+    
+    let dummyData = [
+        
+        "Enjoy trying out new products", "Stay up to date on the latest and greatest in tech and devices","Want to get involved in tech, but not sure where to start", "Enjoy puzzles and challenges", "Enjoy tinkering with computers and devices","Are curious about how tech products work", "Enjoy fast-paced work", "Enjoy solving problems", "Have a creative spirit",
         "Love following the latest web trends & technologies",
         "Think logically and critically",
         "Have tried or are interested in learning how to code", "Are iPhone or Android obsessed", "Are Always on the hunt for the latest cool app",
@@ -22,34 +36,32 @@ class QuizzViewController: UIViewController, UITableViewDelegate, UITableViewDat
         "Have tried or areinterested in learning how to code", "Able to work across teams and with a variety of stakeholders",
         "Communicate effectively and build strong relationships with team members",
         "Have a knack for math, facts and figures",
-        "Love simplifying complex ideas"]
+        "Love simplifying complex ideas"
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.separatorStyle = .none
+        resultButton.layer.cornerRadius = 20
     }
     
-    // Tableview
+    // Tableview Methods
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return dummyData.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: "quizzCell", for: indexPath) as! QuizzCell
+        
         cell.quizLabel.text = dummyData[indexPath.row]
         cell.selectionStyle = .none
-        for button in cell.buttons {
-            button.layer.cornerRadius = 10
-            button.layer.borderColor = UIColor(red: 39/255.0, green: 207/255.0, blue: 171/255.0, alpha: 1).cgColor
-            button.layer.borderWidth = 2
-        }
+        cell.buttonView.layer.cornerRadius = 10
+        
+       
         return cell
     }
     
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print("row \(indexPath.row) is tapped")
-    }
-
 }
 
 class QuizzCell: UITableViewCell {
@@ -57,5 +69,8 @@ class QuizzCell: UITableViewCell {
     @IBOutlet weak var quizLabel: UILabel!
     @IBOutlet weak var buttonView: UIView!
     @IBOutlet var buttons: [UIButton]!
+    @IBOutlet weak var progressLabel: UILabel!
+    @IBOutlet weak var progressView: UIView!
+    
     
 }
