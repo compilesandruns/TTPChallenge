72bb6397526ba67664c5c23957df07f01bea8d60
Luna An <mimicatcodes@gmail.com>
2017-02-16 10:17:27 -0500

    code cleanup a bit + algo needs to be added to quiz results

--- TTPChallenge/QuizVC.swift
+++ TTPChallenge/QuizVC.swift
@@ -11,8 +11,12 @@ import UIKit
 class QuizVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var progressLabel: UILabel!
-    var objectsArray = [QuizGroup]()
-    var counter = 0
+    @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var tableView: UITableView!
+    
+    var objectsArray: [QuizGroup] = []
+    var allQuestions: [[Question]] = []
+    var respondedQuestions = 0
     
     var qa = [String]()
     var it = [String]()
@@ -30,42 +34,42 @@ class QuizVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     let personalityData = [
         
+        //QA
         ["Enjoy trying out new products",
          "Stay up to date on the latest and greatest in tech and devices",
          "Want to get involved in tech, but not sure where to start",
          "Enjoy puzzles and challenges"],
+        //IT
         ["Enjoy tinkering with computers and devices",
          "Are curious about how tech products work",
          "Enjoy fast-paced work",
          "Enjoy solving problems"],
+        //WD
         ["Have a creative spirit",
          "Love following the latest web trends & technologies",
          "Think logically and critically",
          "Have tried or are interested in learning how to code"],
+        //MD
         ["Are iPhone or Android obsessed",
          "Are Always on the hunt for the latest cool app",
          "Have strong preferences for specific technology or apps",
          "Have tried or areinterested in learning how to code"],
+        //DA
         ["Able to work across teams and with a variety of stakeholders",
          "Communicate effectively and build strong relationships with team members",
          "Have a knack for math, facts and figures",
          "Love simplifying complex ideas"]
     ]
     
-    var allQuestions: [[Question]] = []
-    
-    @IBOutlet weak var nextButton: UIButton!
-    @IBOutlet weak var progressBar: UIProgressView!
-    @IBOutlet weak var tableView: UITableView!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         for _ in 1...4 {
-            qa.append("")
-            it.append("")
-            wd.append("")
-            md.append("")
-            da.append("")
+            let emptyStr = ""
+            qa.append(emptyStr)
+            it.append(emptyStr)
+            wd.append(emptyStr)
+            md.append(emptyStr)
+            da.append(emptyStr)
         }
         
         for (i,data) in personalityData.enumerated() {
@@ -153,17 +157,27 @@ class QuizVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     @IBAction func resultButtonTapped(_ sender: UIButton) {
         print("love")
         
-        // logic here - based on the results, it will segue to a different tech type
+        //logic here - based on the results, it will segue to a different tech type
         // algo needed
-        //performSegue(withIdentifier: "resultToWD", sender: self)
+        
+        let t = "1"
+        let truesInQa = qa.filter{$0 == t}.count
+        let truesInIt = it.filter{$0 == t}.count
+        let truesInWd = wd.filter{$0 == t}.count
+        let truesInMd = md.filter{$0 == t}.count
+        let truesInDa = da.filter{$0 == t}.count
+    
+        
         /*
+        
+         performSegue(withIdentifier: "resultToWD", sender: self)
          performSegue(withIdentifier: "resultToDA", sender: self)
          performSegue(withIdentifier: "resultToWD", sender: self)
          performSegue(withIdentifier: "resultToAll", sender: self)
          performSegue(withIdentifier: "resultToMD", sender: self)
          performSegue(withIdentifier: "resultToQA", sender: self)
          
-         */
+        */
     }
     
     
@@ -173,70 +187,40 @@ class QuizVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         case 0:
             print("Quality Assurance Analyst")
             switch row {
-            case 0:
-                qa[0] = "1"
-            case 1:
-                qa[1] = "1"
-            case 2:
-                qa[2] = "1"
-            case 3:
-                qa[3] = "1"
+            case 0, 1, 2, 3:
+                qa[row] = "1"
             default:
                 break
             }
         case 1:
             print("IT Engineer")
             switch row {
-            case 0:
-                it[0] = "1"
-            case 1:
-                it[1] = "1"
-            case 2:
-                it[2] = "1"
-            case 3:
-                it[3] = "1"
+            case 0, 1, 2, 3:
+                it[row] = "1"
             default:
                 break
             }
         case 2:
             print("Web Developer")
             switch row {
-            case 0:
-                wd[0] = "1"
-            case 1:
-                wd[1] = "1"
-            case 2:
-                wd[2] = "1"
-            case 3:
-                wd[3] = "1"
+            case 0, 1, 2, 3:
+                wd[row] = "1"
             default:
                 break
             }
         case 3:
             print("Mobile Developer")
             switch row {
-            case 0:
-                md[0] = "1"
-            case 1:
-                md[1] = "1"
-            case 2:
-                md[2] = "1"
-            case 3:
-                md[3] = "1"
+            case 0, 1, 2, 3:
+                md[row] = "1"
             default:
                 break
             }
         case 4:
             print("Data Analyst")
             switch row {
-            case 0:
-                da[0] = "1"
-            case 1:
-                da[1] = "1"
-            case 2:
-                da[2] = "1"
-            case 3:
-                da[3] = "1"
+            case 0, 1, 2, 3:
+                da[row] = "1"
             default:
                 break
             }
@@ -257,70 +241,40 @@ class QuizVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         case 0:
             print("Quality Assurance Analyst")
             switch row {
-            case 0:
-                qa[0] = "0"
-            case 1:
-                qa[1] = "0"
-            case 2:
-                qa[2] = "0"
-            case 3:
-                qa[3] = "0"
+            case 0, 1, 2, 3:
+                qa[row] = "0"
             default:
                 break
             }
         case 1:
             print("IT Engineer")
             switch row {
-            case 0:
-                it[0] = "0"
-            case 1:
-                it[1] = "0"
-            case 2:
-                it[2] = "0"
-            case 3:
-                it[3] = "0"
+            case 0, 1, 2, 3:
+                it[row] = "0"
             default:
                 break
             }
         case 2:
             print("Web Developer")
             switch row {
-            case 0:
-                wd[0] = "0"
-            case 1:
-                wd[1] = "0"
-            case 2:
-                wd[2] = "0"
-            case 3:
-                wd[3] = "0"
+            case 0, 1, 2, 3:
+                wd[row] = "0"
             default:
                 break
             }
         case 3:
             print("Mobile Developer")
             switch row {
-            case 0:
-                md[0] = "0"
-            case 1:
-                md[1] = "0"
-            case 2:
-                md[2] = "0"
-            case 3:
-                md[3] = "0"
+            case 0, 1, 2, 3:
+                md[row] = "0"
             default:
                 break
             }
         case 4:
             print("Data Analyst")
             switch row {
-            case 0:
-                da[0] = "0"
-            case 1:
-                da[1] = "0"
-            case 2:
-                da[2] = "0"
-            case 3:
-                da[3] = "0"
+            case 0, 1, 2, 3:
+                da[row] = "0"
             default:
                 break
             }
@@ -343,16 +297,17 @@ class QuizVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         let mdCount = md.filter{$0 == ""}.count
         let daCount = da.filter{$0 == ""}.count
         
-        let allEmptyCounts = qaCount + itCount + wdCount + mdCount + daCount
-        counter = 20 - allEmptyCounts
+        let allQuestionsToBeResponded = qa.count + it.count + wd.count + md.count + da.count //20
+        let allEmptyStrs = qaCount + itCount + wdCount + mdCount + daCount
+        respondedQuestions = 20 - allEmptyStrs
     
-        if allEmptyCounts > 0 {
+        if allEmptyStrs > 0 {
             nextButton.isEnabled = false
         } else {
             nextButton.isEnabled = true
         }
   
-        progressLabel.text = "\(counter)/20"
+        progressLabel.text = "\(respondedQuestions)/\(allQuestionsToBeResponded)"
 
     }
 }
