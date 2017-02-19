446ab650775a4c1f499b75603125a165d70c2b38
Stephanie Guevara <stephanie.n.guevara@gmail.com>
2017-02-19 00:20:59 -0500

    wip

--- TTPChallenge/SuggestionViewController.swift
+++ TTPChallenge/SuggestionViewController.swift
@@ -8,11 +8,10 @@
 
 import UIKit
 
-class SuggestionViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UpdateTableView, CustomCellPresentAlert {
+class SuggestionViewController: BaseViewController {
     
     @IBOutlet weak var suggestionTableView: UITableView!
-    
-    @IBOutlet weak var spinner: UIActivityIndicatorView!
+    @IBOutlet var navScrollGestureRecognizer: UIPanGestureRecognizer!
     
     let store = DataStore.shared
     
@@ -32,6 +31,26 @@ class SuggestionViewController: UIViewController, UITableViewDelegate, UITableVi
             }
         }
     }
+}
+
+extension SuggestionViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        guard let cell = tableView.cellForRow(at: indexPath) as? ExpandingMeetUpCell else { return }
+        
+        cell.isExpanded = !cell.isExpanded
+        cell.configureCell()
+        tableView.beginUpdates()
+        tableView.endUpdates()
+        tableView.scrollToRow(at: indexPath, at: .top, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 50.0
+    }
+}
+
+extension SuggestionViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
@@ -75,10 +94,6 @@ class SuggestionViewController: UIViewController, UITableViewDelegate, UITableVi
         }
     }
     
-    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        return 50.0
-    }
-    
     func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
         
         guard let header = view as? UITableViewHeaderFooterView else { return }
@@ -91,18 +106,9 @@ class SuggestionViewController: UIViewController, UITableViewDelegate, UITableVi
         
         return 1
     }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
-        guard let cell = tableView.cellForRow(at: indexPath) as? ExpandingMeetUpCell else { return }
-        
-        cell.isExpanded = !cell.isExpanded
-        cell.configureCell()
-        tableView.beginUpdates()
-        tableView.endUpdates()
-        tableView.scrollToRow(at: indexPath, at: .top, animated: true)
-    }
-    
+}
+
+extension SuggestionViewController : UpdateTableView {
     func updateTableView() {
         OperationQueue.main.addOperation {
             
@@ -110,31 +116,124 @@ class SuggestionViewController: UIViewController, UITableViewDelegate, UITableVi
             self.spinner.stopAnimating()
         }
     }
-    
-    func showAlert(meetup: MeetUp) {
-        
-        let alertController = UIAlertController(title: "See You Later", message: "You're about to leave.", preferredStyle: UIAlertControllerStyle.alert)
-        
-        let DestructiveAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.destructive) {
-            (result : UIAlertAction) -> Void in
+}
 
+extension SuggestionViewController: CustomCellPresentAlert {
+    func showAlert(meetup: MeetUp) {
+        showDecisionAlert(message:  "You're about to leave.", title: "See You Later", okButtonTitle: "Ok", cancelButtonTitle: "Cancel")
+    }
+}
+
+extension SuggestionViewController : UIGestureRecognizerDelegate {
+    func handleNavScrollGesture() {
+        
+        if navScrollGestureRecognizer.state == .Began {
+            navScrollResetPositionY = 0
         }
         
-        let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.default) {
-            (result : UIAlertAction) -> Void in
-            UIApplication.shared.open(URL(string: meetup.url)!, options: [:]) { (success) in
-                
+        var cappedVelocity = navScrollGestureRecognizer.velocityInView(self.view).y
+        if cappedVelocity > kMaxScrollVelocity {
+            cappedVelocity = kMaxScrollVelocity
+        } else if cappedVelocity < -kMaxScrollVelocity {
+            cappedVelocity = -kMaxScrollVelocity
+        }
+        
+        var finalPlanHeaderHeight = planHeaderViewHeightConstraint.constant + cappedVelocity / ((kExpandedHeaderHeight-kContractedHeaderHeight))
+        if finalPlanHeaderHeight > kExpandedHeaderHeight {
+            finalPlanHeaderHeight = kExpandedHeaderHeight
+        } else if finalPlanHeaderHeight < kContractedHeaderHeight {
+            finalPlanHeaderHeight = kContractedHeaderHeight
+        }
+        
+        if cappedVelocity < 0 {
+            if tableView.contentOffset.y >= 0 && finalPlanHeaderHeight >= kContractedHeaderHeight {
+                planHeaderViewHeightConstraint.constant = finalPlanHeaderHeight
+            }
+        } else if cappedVelocity > 0 {
+            if tableView.contentOffset.y <= 0 && finalPlanHeaderHeight <= kExpandedHeaderHeight {
+                planHeaderViewHeightConstraint.constant = finalPlanHeaderHeight
             }
         }
         
-        alertController.addAction(DestructiveAction)
-        alertController.addAction(okAction)
+        var finalLargeLogoHeight = largeLogoViewHeightConstraint.constant + cappedVelocity / ((kExpandedLogoHeight-kContractedLogoHeight)*kLargeLogoDistanceMultiplier)
         
-        present(alertController, animated: true, completion: nil)
+        if finalLargeLogoHeight > kExpandedLogoHeight {
+            finalLargeLogoHeight = kExpandedLogoHeight
+        } else if finalLargeLogoHeight < kContractedLogoHeight {
+            finalLargeLogoHeight = kContractedLogoHeight
+        }
+        
+        if cappedVelocity < 0 {
+            if tableView.contentOffset.y >= 0 && finalLargeLogoHeight >= kContractedLogoHeight {
+                largeLogoViewHeightConstraint.constant = finalLargeLogoHeight
+            }
+        } else if cappedVelocity > 0 {
+            if tableView.contentOffset.y <= 0 && finalLargeLogoHeight <= kExpandedLogoHeight {
+                largeLogoViewHeightConstraint.constant = finalLargeLogoHeight
+            }
+        }
+        
+        let headerPercentageOffset = (kExpandedHeaderHeight - planHeaderViewHeightConstraint.constant) / (kExpandedHeaderHeight - kContractedHeaderHeight)
+        
+        if headerPercentageOffset >= 0.999 {
+            largeMembershipIdLabel.alpha = 1
+            memberNumberStringLabel.alpha = 0
+            membershipIdLabel.alpha = 0
+            planNameLabel.alpha = 0
+        } else if headerPercentageOffset <= 0.001 {
+            largeMembershipIdLabel.alpha = 0
+            memberNumberStringLabel.alpha = 1
+            membershipIdLabel.alpha = 1
+            planNameLabel.alpha = 1
+        } else {
+            
+            if headerPercentageOffset > 0.5 {
+                largeMembershipIdLabel.alpha = (2 * CGFloat(headerPercentageOffset)) - 1
+            } else {
+                memberNumberStringLabel.alpha = 1 - (2 * CGFloat(headerPercentageOffset))
+                membershipIdLabel.alpha = 1 - (2 * CGFloat(headerPercentageOffset))
+                planNameLabel.alpha = 1 - (2 * CGFloat(headerPercentageOffset))
+            }
+        }
+        
+        if navScrollGestureRecognizer.state == .Ended || navScrollGestureRecognizer.state == .Cancelled  {
+            if (headerPercentageOffset > 0.5 && cappedVelocity > -30 && cappedVelocity < 30) || cappedVelocity < -30 {
+                let pixels = min(planHeaderViewHeightConstraint.constant - kContractedHeaderHeight, (largeLogoViewHeightConstraint.constant - kContractedHeaderHeight)*kLargeLogoDistanceMultiplier)
+                
+                self.view.layoutIfNeeded()
+                
+                UIView.animateWithDuration(Double(pixels/kMaxScrollVelocity), animations: { [unowned self] in
+                    self.largeMembershipIdLabel.alpha = 1
+                    self.memberNumberStringLabel.alpha = 0
+                    self.membershipIdLabel.alpha = 0
+                    self.planNameLabel.alpha = 0
+                    
+                    self.largeLogoViewHeightConstraint.constant = self.kContractedLogoHeight
+                    self.planHeaderViewHeightConstraint.constant = self.kContractedHeaderHeight
+                    
+                    self.view.layoutIfNeeded()
+                })
+            } else {
+                let pixels = min(kExpandedHeaderHeight - planHeaderViewHeightConstraint.constant, (kExpandedLogoHeight - largeLogoViewHeightConstraint.constant)*kLargeLogoDistanceMultiplier)
+                
+                self.view.layoutIfNeeded()
+                
+                UIView.animateWithDuration(Double(pixels/kMaxScrollVelocity), animations: { [unowned self] in
+                    self.largeMembershipIdLabel.alpha = 0
+                    self.memberNumberStringLabel.alpha = 1
+                    self.membershipIdLabel.alpha = 1
+                    self.planNameLabel.alpha = 1
+                    
+                    self.largeLogoViewHeightConstraint.constant = self.kExpandedLogoHeight
+                    self.planHeaderViewHeightConstraint.constant = self.kExpandedHeaderHeight
+                    
+                    self.view.layoutIfNeeded()
+                })
+            }
+        }
     }
     
+    func gestureRecognizer(gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWithGestureRecognizer otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
+    }
 }
-
-
-
-
